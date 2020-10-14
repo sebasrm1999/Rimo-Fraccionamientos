f6531c6682b140340523ffda260f92d5314b27bd
Sebas Ramos <sebasrm69@gmail.com>
2020-10-14 00:21:50 -0500

    cruds agregados

--- application/controllers/BackEnd.php
+++ application/controllers/BackEnd.php
@@ -10,6 +10,8 @@ class BackEnd extends CI_Controller{
 
     }
 
+    //AREAS
+
     public function nuevaarea(){
         $nombre    = $this->input->post( "nombre" ); 
         $encargado      = $this->input->post( "encargado" );
@@ -49,5 +51,470 @@ class BackEnd extends CI_Controller{
         $this->output->set_content_type( "application/json" );
         echo json_encode( $obj );
     }
+
+    public function borraarea(){
+        $id    = $this->input->post( "id" );
+
+        $obj = $this->BackEnd_model->delete_area($id);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    //PREGUNTAS
+
+    public function nuevapregunta(){
+        $asunto    = $this->input->post( "asunto" ); 
+        $desc      = $this->input->post( "descripcion" );
+
+        $data = array(
+            'id_pregunta' => 0,
+            'asunto' => $asunto,
+            'descripcion' => $desc
+        );
+
+        $obj = $this->BackEnd_model->inserta_pregunta($data);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function preguntas(){
+        $obj = $this->BackEnd_model->get_preguntas();
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function pregunta(){
+        $id    = $this->input->post( "id" ); 
+        $obj = $this->BackEnd_model->get_pregunta($id);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function actualizapregunta(){
+        $id    = $this->input->post( "id" );
+        $asunto    = $this->input->post( "asunto" ); 
+        $descripcion      = $this->input->post( "descripcion" );
+
+        $data = array(
+            'id_pregunta' => $id,
+            'asunto' => $asunto,
+            'descripcion' => $descripcion
+        );
+
+        $obj = $this->BackEnd_model->update_pregunta($data);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function borrapregunta(){
+        $id    = $this->input->post( "id" );
+
+        $obj = $this->BackEnd_model->delete_pregunta($id);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    //USUARIOS
+
+    public function nuevousuario(){
+        $nombre    = $this->input->post( "nombre" ); 
+        $duenio      = $this->input->post( "duenio" );
+        $correo    = $this->input->post( "correo" ); 
+        $contrasenia      = $this->input->post( "contrasenia" );
+        $telefono    = $this->input->post( "telefono" ); 
+        $direccion      = $this->input->post( "direccion" );
+
+        $data = array(
+            'id_usuario' => 0,
+            'nombre' => $nombre,
+            'duenio' => $duenio,
+            'tipo' => 1,
+            'correo' => $correo,
+            'contrasenia' => $contrasenia,
+            'telefono' => $telefono,
+            'direccion' => $direccion,
+            'verificado' => 0,
+        );
+
+        $obj = $this->BackEnd_model->inserta_usuario($data);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function usuarios(){
+        $obj = $this->BackEnd_model->get_usuarios();
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function usuario(){
+        $id    = $this->input->post( "id" ); 
+        $obj = $this->BackEnd_model->get_usuario($id);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function actualizausuario(){
+        $id    = $this->input->post( "id" ); 
+        $nombre    = $this->input->post( "nombre" ); 
+        $duenio      = $this->input->post( "duenio" );
+        $correo    = $this->input->post( "correo" ); 
+        $contrasenia      = $this->input->post( "contrasenia" );
+        $telefono    = $this->input->post( "telefono" ); 
+        $direccion      = $this->input->post( "direccion" );
+        $verificado      = $this->input->post( "verificado" );
+
+        $data = array(
+            'id_usuario' => $id,
+            'nombre' => $nombre,
+            'duenio' => $duenio,
+            'correo' => $correo,
+            'contrasenia' => $contrasenia,
+            'telefono' => $telefono,
+            'direccion' => $direccion,
+            'verificado' => $verificado,
+        );
+
+        $obj = $this->BackEnd_model->update_usuario($data);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function borrausuario(){
+        $id    = $this->input->post( "id" );
+
+        $obj = $this->BackEnd_model->delete_usuario($id);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    //AVISOS
+
+    public function nuevoaviso(){
+        $tipo    = $this->input->post( "tipo" ); 
+        $asunto      = $this->input->post( "asunto" );
+        $desc    = $this->input->post( "descripcion" ); 
+        $fecha      = date("Y-m-d");
+        $hora    = date("H:i:s");
+
+        if($tipo == 2){
+            $idusu    = $this->input->post( "id_usuario" );
+
+            $data = array(
+            'id_aviso' => 0,
+            'tipo' => $tipo,
+            'id_usuario' => $idusu,
+            'status' => 0,
+            'asunto' => $asunto,
+            'descripcion' => $desc,
+            'fecha' => $fecha,
+            'hora' => $hora
+            ); 
+
+            $obj = $this->BackEnd_model->inserta_aviso($data);
+
+            $this->output->set_content_type( "application/json" );
+            echo json_encode( $obj );
+        } else if($tipo == 1){
+
+            $data = array(
+            'id_aviso' => 0,
+            'tipo' => $tipo,
+            'status' => 0,
+            'asunto' => $asunto,
+            'descripcion' => $desc,
+            'fecha' => $fecha,
+            'hora' => $hora
+            ); 
+
+            $obj = $this->BackEnd_model->inserta_aviso($data);
+
+            $this->output->set_content_type( "application/json" );
+            echo json_encode( $obj );
+        } else {
+            $obj['mensaje'] = 'Tipo inválido';
+
+            $this->output->set_content_type( "application/json" );
+            echo json_encode( $obj );
+        }
+
+        
+    }
+
+    public function avisos(){
+        $obj = $this->BackEnd_model->get_avisos();
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function aviso(){
+        $id    = $this->input->post( "id" ); 
+        $obj = $this->BackEnd_model->get_aviso($id);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function actualizaaviso(){
+        $id    = $this->input->post( "id" );
+        $asunto      = $this->input->post( "asunto" );
+        $desc    = $this->input->post( "descripcion" );
+        $status    = $this->input->post( "status" );
+
+        $data = array(
+            'id_aviso' => $id,
+            'status' => $status,
+            'asunto' => $asunto,
+            'descripcion' => $desc
+            ); 
+
+        $obj = $this->BackEnd_model->update_aviso($data);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function borraaviso(){
+        $id    = $this->input->post( "id" );
+
+        $obj = $this->BackEnd_model->delete_aviso($id);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    //PAGOS
+
+    public function nuevopago(){
+        $id    = $this->input->post( "id_usuario" ); 
+        $mes      = $this->input->post( "mes" );
+        $anio    = $this->input->post( "anio" );
+
+        $data = array(
+            'id_pago' => 0,
+            'id_usuario' => $id,
+            'mes' => $mes,
+            'anio' => $anio
+        );
+
+        $obj = $this->BackEnd_model->inserta_pago($data);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function actualizapago(){
+        $id    = $this->input->post( "id" );
+        $idusu      = $this->input->post( "id_usuario" );
+        $mes    = $this->input->post( "mes" );
+        $anio    = $this->input->post( "anio" );
+        $fecha    = $this->input->post( "fecha" );
+        $hora    = $this->input->post( "hora" );
+        $pronto    = $this->input->post( "pronto" );
+        $status    = $this->input->post( "status" );
+        $tipo    = $this->input->post( "tipo" );
+
+        $data = array(
+            'id_pago' => $id,
+            'fecha' => $fecha,
+            'hora' => $hora,
+            'id_usuario' => $idusu,
+            'mes' => $mes,
+            'anio' => $anio,
+            'pronto' => $pronto,
+            'status' => $status,
+            'tipo' => $tipo,
+            ); 
+
+        $obj = $this->BackEnd_model->update_pago($data);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function pagos(){
+        $obj = $this->BackEnd_model->get_pagos();
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function pago(){
+        $id    = $this->input->post( "id" ); 
+        $obj = $this->BackEnd_model->get_pago($id);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function borrapago(){
+        $id    = $this->input->post( "id" );
+
+        $obj = $this->BackEnd_model->delete_pago($id);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    //QUEJAS
+
+    public function nuevaqueja(){
+        $asunto    = $this->input->post( "asunto" ); 
+        $desc      = $this->input->post( "descripcion" );
+        $idarea    = $this->input->post( "id_area" );
+        $fecha      = date("Y-m-d");
+        $hora    = date("H:i:s");
+        $idusu    = $this->input->post( "id_usuario" );
+
+        $data = array(
+            'id_queja' => 0,
+            'asunto' => $asunto,
+            'descripcion' => $desc,
+            'id_area' => $idarea,
+            'fecha' => $fecha,
+            'hora' => $hora,
+            'id_usuario' => $idusu,
+        );
+
+        $obj = $this->BackEnd_model->inserta_queja($data);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function quejas(){
+        $obj = $this->BackEnd_model->get_quejas();
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function queja(){
+        $id    = $this->input->post( "id" ); 
+        $obj = $this->BackEnd_model->get_queja($id);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function actualizaqueja(){
+        $id    = $this->input->post( "id" );
+        $idusu      = $this->input->post( "id_usuario" );
+        $asunto    = $this->input->post( "asunto" );
+        $descripcion    = $this->input->post( "descripcion" );
+        $fecha    = $this->input->post( "fecha" );
+        $hora    = $this->input->post( "hora" );
+        $idarea    = $this->input->post( "id_area" );
+        $status    = $this->input->post( "status" );
+
+        $data = array(
+            'id_queja' => $id,
+            'fecha' => $fecha,
+            'hora' => $hora,
+            'id_usuario' => $idusu,
+            'asunto' => $asunto,
+            'descripcion' => $descripcion,
+            'id_area' => $idarea,
+            'status' => $status,
+            ); 
+
+        $obj = $this->BackEnd_model->update_queja($data);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function borraqueja(){
+        $id    = $this->input->post( "id" );
+
+        $obj = $this->BackEnd_model->delete_queja($id);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    //COMENTARIOS
+
+    public function nuevocomentario(){
+        $idusu    = $this->input->post( "id_usuario" ); 
+        $texto      = $this->input->post( "texto" );
+        $idqueja    = $this->input->post( "id_queja" );
+        $fecha      = date("Y-m-d");
+        $hora    = date("H:i:s");
+
+        $data = array(
+            'id_comentario' => 0,
+            'id_usuario' => $idusu,
+            'texto' => $texto,
+            'fecha' => $fecha,
+            'hora' => $hora,
+            'id_queja' => $idqueja,
+        );
+
+        $obj = $this->BackEnd_model->inserta_comentario($data);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function comentarios(){
+        $obj = $this->BackEnd_model->get_comentarios();
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function comentario(){
+        $id    = $this->input->post( "id" ); 
+        $obj = $this->BackEnd_model->get_comentario($id);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function actualizacomentario(){
+        $id    = $this->input->post( "id" );
+        $idusu      = $this->input->post( "id_usuario" );
+        $texto    = $this->input->post( "texto" );
+        $fecha    = $this->input->post( "fecha" );
+        $hora    = $this->input->post( "hora" );
+        $idqueja    = $this->input->post( "id_queja" );
+
+        $data = array(
+            'id_comentario' => $id,
+            'fecha' => $fecha,
+            'hora' => $hora,
+            'id_usuario' => $idusu,
+            'texto' => $texto,
+            'id_queja' => $idqueja
+            ); 
+
+        $obj = $this->BackEnd_model->update_comentario($data);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
+    public function borracomentario(){
+        $id    = $this->input->post( "id" );
+
+        $obj = $this->BackEnd_model->delete_comentario($id);
+
+        $this->output->set_content_type( "application/json" );
+        echo json_encode( $obj );
+    }
+
 }
 ?>
--- application/models/BackEnd_model.php
+++ application/models/BackEnd_model.php
@@ -4,6 +4,8 @@ class BackEnd_model extends CI_Model{
     public function __construct(){
 		parent::__construct();
     }
+
+    //AREAS
     
     public function inserta_area($data){
         $this->db->insert('area', $data);
@@ -31,6 +33,259 @@ class BackEnd_model extends CI_Model{
 
         $this->db->where('id_area', $id);
         $rs = $this->db->get('area');
+
+        $obj["resultado"] = $rs->num_rows() == 0 ? TRUE : FALSE;
+
+        return $obj;
+    }
+
+    //PREGUNTAS
+
+    public function inserta_pregunta($data){
+        $this->db->insert('pregunta', $data);
+        $obj["resultado"] = $this->db->affected_rows() > 0;
+        return $obj;
+    }
+
+    public function update_pregunta($data){
+        $this->db->where('id_pregunta', $data['id_pregunta']);
+        $this->db->update('pregunta', $data);
+        $obj["resultado"] = $this->db->affected_rows() > 0;
+        return $obj;
+    }
+
+    public function get_preguntas(){
+        $rs = $this->db->get("pregunta");
+        $obj['preguntas'] = $rs->num_rows() == 0 ? NULL : $rs->result(); 
+
+		return $obj;
+    }
+
+    public function get_pregunta($id){
+        $this->db->where('id_pregunta', $id);
+        $rs = $this->db->get("pregunta");
+        $obj['pregunta'] = $rs->num_rows() == 0 ? NULL : $rs->result(); 
+
+		return $obj;
+    }
+
+    public function delete_pregunta($id){
+        $this->db->where('id_pregunta', $id);
+        $this->db->delete('pregunta');
+
+        $this->db->where('id_pregunta', $id);
+        $rs = $this->db->get('pregunta');
+
+        $obj["resultado"] = $rs->num_rows() == 0 ? TRUE : FALSE;
+
+        return $obj;
+    }
+
+    //USUARIOS
+
+    public function inserta_usuario($data){
+        $this->db->insert('usuario', $data);
+        $obj["resultado"] = $this->db->affected_rows() > 0;
+        return $obj;
+    }
+
+    public function update_usuario($data){
+        $this->db->where('id_usuario', $data['id_usuario']);
+        $this->db->update('usuario', $data);
+        $obj["resultado"] = $this->db->affected_rows() > 0;
+        return $obj;
+    }
+
+    public function get_usuarios(){
+        $this->db->where('tipo', 1);
+        $rs = $this->db->get("usuario");
+        $obj['usuarios'] = $rs->num_rows() == 0 ? NULL : $rs->result(); 
+
+		return $obj;
+    }
+
+    public function get_usuario($id){
+        $this->db->where('id_usuario', $id);
+        $rs = $this->db->get("usuario");
+        $obj['usuario'] = $rs->num_rows() == 0 ? NULL : $rs->result(); 
+
+		return $obj;
+    }
+
+    public function delete_usuario($id){
+        $this->db->where('id_usuario', $id);
+        $this->db->delete('usuario');
+
+        $this->db->where('id_usuario', $id);
+        $rs = $this->db->get('usuario');
+
+        $obj["resultado"] = $rs->num_rows() == 0 ? TRUE : FALSE;
+
+        return $obj;
+    }
+
+    //AVISOS
+
+    public function inserta_aviso($data){
+        $this->db->insert('aviso', $data);
+        $obj["resultado"] = $this->db->affected_rows() > 0;
+        return $obj;
+    }
+
+    public function update_aviso($data){
+        $this->db->where('id_aviso', $data['id_aviso']);
+        $this->db->update('aviso', $data);
+        $obj["resultado"] = $this->db->affected_rows() > 0;
+        return $obj;
+    }
+
+    public function get_avisos(){
+        $rs = $this->db->get("aviso");
+        $obj['avisos'] = $rs->num_rows() == 0 ? NULL : $rs->result(); 
+
+		return $obj;
+    }
+
+    public function get_aviso($id){
+        $this->db->where('id_aviso', $id);
+        $rs = $this->db->get("aviso");
+        $obj['aviso'] = $rs->num_rows() == 0 ? NULL : $rs->result(); 
+
+		return $obj;
+    }
+
+    public function delete_aviso($id){
+        $this->db->where('id_aviso', $id);
+        $this->db->delete('aviso');
+
+        $this->db->where('id_aviso', $id);
+        $rs = $this->db->get('aviso');
+
+        $obj["resultado"] = $rs->num_rows() == 0 ? TRUE : FALSE;
+
+        return $obj;
+    }
+
+    //PAGOS
+
+    public function inserta_pago($data){
+        $this->db->insert('pago', $data);
+        $obj["resultado"] = $this->db->affected_rows() > 0;
+        return $obj;
+    }
+
+    public function update_pago($data){
+        $this->db->where('id_pago', $data['id_pago']);
+        $this->db->update('pago', $data);
+        $obj["resultado"] = $this->db->affected_rows() > 0;
+        return $obj;
+    }
+
+    public function get_pagos(){
+        $rs = $this->db->get("pago");
+        $obj['pagos'] = $rs->num_rows() == 0 ? NULL : $rs->result(); 
+
+		return $obj;
+    }
+
+    public function get_pago($id){
+        $this->db->where('id_pago', $id);
+        $rs = $this->db->get("pago");
+        $obj['pago'] = $rs->num_rows() == 0 ? NULL : $rs->result(); 
+
+		return $obj;
+    }
+
+    public function delete_pago($id){
+        $this->db->where('id_pago', $id);
+        $this->db->delete('pago');
+
+        $this->db->where('id_pago', $id);
+        $rs = $this->db->get('pago');
+
+        $obj["resultado"] = $rs->num_rows() == 0 ? TRUE : FALSE;
+
+        return $obj;
+    }
+
+    //QUEJAS
+
+    public function inserta_queja($data){
+        $this->db->insert('queja', $data);
+        $obj["resultado"] = $this->db->affected_rows() > 0;
+        return $obj;
+    }
+
+    public function update_queja($data){
+        $this->db->where('id_queja', $data['id_queja']);
+        $this->db->update('queja', $data);
+        $obj["resultado"] = $this->db->affected_rows() > 0;
+        return $obj;
+    }
+
+    public function get_quejas(){
+        $rs = $this->db->get("queja");
+        $obj['quejas'] = $rs->num_rows() == 0 ? NULL : $rs->result(); 
+
+		return $obj;
+    }
+
+    public function get_queja($id){
+        $this->db->where('id_queja', $id);
+        $rs = $this->db->get("queja");
+        $obj['queja'] = $rs->num_rows() == 0 ? NULL : $rs->result(); 
+
+		return $obj;
+    }
+
+    public function delete_queja($id){
+        $this->db->where('id_queja', $id);
+        $this->db->delete('queja');
+
+        $this->db->where('id_queja', $id);
+        $rs = $this->db->get('queja');
+
+        $obj["resultado"] = $rs->num_rows() == 0 ? TRUE : FALSE;
+
+        return $obj;
+    }
+
+    //COMENTARIOS
+
+    public function inserta_comentario($data){
+        $this->db->insert('comentario', $data);
+        $obj["resultado"] = $this->db->affected_rows() > 0;
+        return $obj;
+    }
+
+    public function update_comentario($data){
+        $this->db->where('id_comentario', $data['id_comentario']);
+        $this->db->update('comentario', $data);
+        $obj["resultado"] = $this->db->affected_rows() > 0;
+        return $obj;
+    }
+
+    public function get_comentarios(){
+        $rs = $this->db->get("comentario");
+        $obj['comentarios'] = $rs->num_rows() == 0 ? NULL : $rs->result(); 
+
+		return $obj;
+    }
+
+    public function get_comentario($id){
+        $this->db->where('id_comentario', $id);
+        $rs = $this->db->get("comentario");
+        $obj['comentario'] = $rs->num_rows() == 0 ? NULL : $rs->result(); 
+
+		return $obj;
+    }
+
+    public function delete_comentario($id){
+        $this->db->where('id_comentario', $id);
+        $this->db->delete('comentario');
+
+        $this->db->where('id_comentario', $id);
+        $rs = $this->db->get('comentario');
 
         $obj["resultado"] = $rs->num_rows() == 0 ? TRUE : FALSE;
 
