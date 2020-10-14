01994f15f9fac5209a9d28630d8fa213cf9219f5
Sebas Ramos <sebasrm69@gmail.com>
2020-10-14 00:55:18 -0500

    login colonos

--- static/js/login.js
+++ static/js/login.js
@@ -1,10 +1,31 @@
 let base_url = 'http://localhost/myhome_ci/';
 
 function login(){
+    let error = document.getElementById('password-error');
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
-    sessionStorage.setItem("id", '1');
-    sessionStorage.setItem("correo", email);
 
-    window.location.replace(`${base_url}index.php/inicio`);
+    error.innerHTML = '';
+    $.ajax({
+        "url" : base_url + "BackEnd/login",
+        "type" : "post",
+        "data" : {
+            "correo" : email,
+            "contrasenia" : password
+        },
+        "dataType" : "json",
+        "success" : function(json){
+
+            if(json.resultado){
+                sessionStorage.setItem("id", json.usuario[0].id_usuario);
+                sessionStorage.setItem("correo", email);
+
+                window.location.replace(`${base_url}index.php/inicio`);
+            } else {
+                error.innerHTML = 'Correo o contraseña incorrectos';
+            }
+            
+        }
+    });
+    
 }
